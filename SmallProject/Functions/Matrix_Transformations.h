2638f4a7cc53a1fe027714288a754a33244fdbe5
rastabrandy02 <73582888+rastabrandy02@users.noreply.github.com>
2022-01-08 13:50:14 +0100

    Also included functions to swap between components

--- SmallProject/Functions/Matrix_Transformations.h
+++ SmallProject/Functions/Matrix_Transformations.h
@@ -1,5 +1,5 @@
 #pragma once
-
+//--------GET THE MATRIX---------
 //From euler Axis and Angle to Matrix
 void GetMatrixFromAxisAngle(float* axis, float angle, float matrix[][3])
 {
@@ -80,5 +80,62 @@ void GetAxisAngleFromVector(float* vector, float* axis, float& angle)
 	for (int i = 0; i < 3; i++)
 	{
 		axis[i] = vector[i] / angle;
+	}
+}
+
+
+//---------GET THE COMPONENTS FROM THE MATRIX---------
+//From Matrix to Euler Angles
+void GetEulerAnglesFromMatrix(float matrix[][3], float& alpha, float& beta, float& gamma)
+{
+	beta = RADTODEG * (-asin(matrix[2][0]));
+	gamma = RADTODEG * atan2f(matrix[0][0] / cos(beta), matrix[1][0] / cos(beta));
+	alpha = RADTODEG * atan2f(matrix[2][1] / cos(beta), matrix[2][2] / cos(beta));
+	if (alpha < 0) alpha = alpha + 180;
+	if (gamma < 0) gamma = gamma + 180;
+}
+//From Matrix to Euler Axis and Angle 
+void GetAxisAngleFromMatrix(float matrix[][3], float* axis, float& angle)
+{
+	float transposed[3][3] = { 0 };
+	float matrixVectorProduct[3][3] = { 0 };
+
+	angle = acos((GetTrace(matrix) - 1) / 2);
+	while (angle * RADTODEG > 360) angle -= 360 * DEGTORAD;
+
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			transposed[i][j] = matrix[j][i];
+		}
+	}
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			matrixVectorProduct[i][j] = (matrix[i][j] - transposed[i][j]) / (2 * sin(angle));
+		}
+	}
+	axis[0] = (matrixVectorProduct[2][1] - matrixVectorProduct[1][2]) / 2;
+	axis[1] = (matrixVectorProduct[0][2] - matrixVectorProduct[2][0]) / 2;
+	axis[2] = (matrixVectorProduct[1][0] - matrixVectorProduct[0][1]) / 2;
+}
+
+//From Matrix to Euler Axis and Angle, then from Euler Axis and Angle to Quaternion
+void GetQuaternionFromAxisAngle(float* axis, float angle, float* quaternion)
+{
+	quaternion[0] = cos(angle / 2);
+	for (int i = 0; i < 3; i++)
+	{
+		quaternion[i + 1] = axis[i] * sin(angle / 2);
+	}
+}
+//From Matrix to Euler Axis and Angle, then from Euler Axis and Angle to Rotation Vector
+void GetVectorFromAxisAngle(float* axis, float angle, float* vector)
+{
+	for (int i = 0; i < 3; i++)
+	{
+		vector[i] = angle * axis[i];
 	}
 }
